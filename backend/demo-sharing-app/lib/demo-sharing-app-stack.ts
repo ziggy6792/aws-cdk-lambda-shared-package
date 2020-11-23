7c01d694c06e887aefab0faa5d5e69dbcd2f1aa2
Simon <simonaverhoeven@gmail.com>
2020-11-24 03:04:21 +0800

    add lambda to stack

--- backend/demo-sharing-app/lib/demo-sharing-app-stack.ts
+++ backend/demo-sharing-app/lib/demo-sharing-app-stack.ts
@@ -5,6 +5,12 @@ export class DemoSharingAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const helloLambda = new lambda.Function(this, 'HelloWorldLambda', {
+      code: lambda.Code.fromAsset('functions/hello-world/'),
+      handler: 'index.handler',
+      runtime: lambda.Runtime.NODEJS_12_X,
+    });
+
     // The code that defines your stack goes here
   }
 }

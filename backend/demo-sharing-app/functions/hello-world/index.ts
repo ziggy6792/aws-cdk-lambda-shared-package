19abf43ebbdf4f5e58f0c38dab184ca04fb4748f
Simon <simonaverhoeven@gmail.com>
2020-11-24 03:15:47 +0800

    shared lib to functions

--- backend/demo-sharing-app/functions/hello-world/index.ts
+++ backend/demo-sharing-app/functions/hello-world/index.ts
@@ -1,9 +1,14 @@
 import { v4 as uuid } from 'uuid';
+import mylib from 'my-lib/MyLib';
 
 exports.handler = async function (event: AWSLambda.APIGatewayEvent) {
   console.log('event', JSON.stringify(event, null, 0));
 
   console.log(`here is a uuid ${uuid()}`);
+
+  const result = mylib();
+
+  console.log(`result from lib is ${result}`);
 
   return {
     statusCode: 200,
